fc2deb78991e940d83d0b1402b065711ce84d329
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-08-07 10:44:51 +0200

    create user.java class

--- src/main/java/it/unibo/ristoDB/db/Employee.java
+++ src/main/java/it/unibo/ristoDB/db/Employee.java
@@ -5,15 +5,11 @@ public class Employee {
     private int id;
     private String name;
     private String surname;
-    private String username;
-    private String password;
 
-    public Employee(int id, String name, String surname, String username, String password) {
+    public Employee(int id, String name, String surname) {
         this.id = id;
         this.name = name;
         this.surname = surname;
-        this.username = username;
-        this.password = password;
     }
 
     public int getId() {
